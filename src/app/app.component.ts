1cbc146dc3e9bb45f2e5ef5a82fde916337e5095
CyberNicky <nickyalbuquerque2008@gmail.com>
2023-01-02 16:05:30 -0300

    feat: initializing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Aula de Services</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
