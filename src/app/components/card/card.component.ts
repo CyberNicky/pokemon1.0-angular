c55b0fe78641fdd6c48eadb73a64b9b7753e0053
CyberNicky <nickyalbuquerque2008@gmail.com>
2023-01-02 18:06:20 -0300

    feat: add search pokemon

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,18 +1,38 @@
+import { PokemonData } from './../../models/pokemonData';
+import { PokemonService } from './../../services/pokemon.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  name:string = 'BULBASSAUR'
-  attributesTypes:string[] = ['FIRE', 'ROCK']
-  constructor(){
-
-  }
+  pokemon: PokemonData 
+  constructor(private service: PokemonService) {
+    this.pokemon = {
+        id:0,
+        name: '',
+        sprites: {
+          front_default: ''
+        },
+        types: []
+      }
+    }
   ngOnInit(): void {
-   
+    this.getPokemon('pikachu')
   }
-
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id:res.id,
+          name:res.name,
+          sprites:res.sprites,
+          types:res.types
+        }
+      },
+      error: (err) => console.log('not found'),
+    })
+  }
 }
