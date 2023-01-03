f0b118830ee1723502f748a917791003a856eba3
CyberNicky <nickyalbuquerque2008@gmail.com>
2023-01-03 14:32:02 -0300

    feat: change of templateURL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Aula de Services</h1>
-  <app-card></app-card>
-  <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
