c70638a62e38b7fb5f8ad325698062cabebb7c65
CyberNicky <nickyalbuquerque2008@gmail.com>
2023-01-03 14:36:38 -0300

    feat:importing Httpclient

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,29 +1,20 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "src/environments/environment"
-import { PokemonData } from "../models/pokemonData"
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { PokemonData } from '../models/pokemonData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-  export class PokemonService {
-    private baseURL:string = ""
-    private pokeData:PokemonData | any
-    constructor(
-      private http: HttpClient
-    ){
-      this.baseURL = environment.pokeApi
-
-    }
-    getPokemon(pokemonName:string):Observable<PokemonData>{
-     this.pokeData = this
-     .http
-     .get<PokemonData>
-     (`${this.baseURL}${pokemonName}`)
-     return this.pokeData
-
-    }
-
+export class PokemonService {
+  private baseURL: string = '';
+  private pokeData: PokemonData | any;
+  constructor(private http: HttpClient) {
+    this.baseURL = environment.pokeApi;
   }
+  getPokemon(pokemonName: string): Observable<PokemonData> {
+    this.pokeData = this.http.get<PokemonData>(`${this.baseURL}${pokemonName}`);
+    return this.pokeData;
+  }
+}
